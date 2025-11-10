a0716cebd42066100847659bb4afcefeef03ff9e
abanoubmamdou7 <abanoub.mamdouh@smartupeg.com>
2025-11-10 16:23:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "modules", "files")));
 
 const corsOptions = {
-  origin: "https://adams-lounge.com",
+  origin: "https://menu-fe-eta.vercel.app",
   // origin: "http://localhost:8080",
 
   credentials: true,
